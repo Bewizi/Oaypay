f5ac20e7d9c85d7f1672a5ae2607ba23750b3555
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-04-17 22:56:21 +0100

    Add AppButton component with styling and default text

---
+++ lib/components/app_button.dart
@@ -0,0 +1,15 @@
+import 'package:jaspr/dom.dart';
+import 'package:jaspr/jaspr.dart';
+
+class AppButton extends StatelessComponent {
+  const AppButton({super.key});
+
+  @override
+  Component build(BuildContext context) {
+    return button(
+      classes:
+          'text-dark-slate-grey-800 bg-lime-cream-300 hover:bg-lime-cream-400 py-5 px-12 rounded-full font-medium text-2xl mt-14',
+      [.text('Get started for free')],
+    );
+  }
+}
