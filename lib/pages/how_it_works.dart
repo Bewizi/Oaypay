4d81080462cb2e51a1f24f929bfdf56ff99835ca
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-04-21 23:41:21 +0100

    Add detailed steps to HowItWorks component for improved user guidance

--- lib/pages/how_it_works.dart
+++ lib/pages/how_it_works.dart
@@ -1,10 +1,32 @@
 import 'package:jaspr/dom.dart';
 import 'package:jaspr/jaspr.dart';
+import 'package:oaypay/components/app_button.dart';
 import 'package:oaypay/components/conatiner.dart';
 import 'package:oaypay/components/main_headers.dart';
 
 class HowItWorks extends StatelessComponent {
   const HowItWorks({super.key});
+
+  final List<Map<String, String>> steps = const [
+    {
+      'icon': 'ic_sharp-download.svg',
+      'alt_text': 'Sharp Download Icon',
+      'title': 'Download App & Sign Up',
+      'description': 'First download our app on apple store or google store and register ',
+    },
+    {
+      'icon': 'bitcoin-icons_verify-filled.svg',
+      'alt_text': 'Verify Icon',
+      'title': 'Verify Your Account',
+      'description': 'Provide any means of identification to verify your details to secure account',
+    },
+    {
+      'icon': 'cib_sparkfun.svg',
+      'alt_text': 'Services Icon',
+      'title': 'Start Enjoying Services',
+      'description': 'Enjoy our unlimited services like send, transfer, receive, pay bills and more',
+    },
+  ];
 
   @override
   Component build(BuildContext context) {
@@ -16,6 +38,28 @@ class HowItWorks extends StatelessComponent {
             description: 'A free bank account to power your financial',
             text: 'Easy 3 steps to use our services',
           ),
+
+          section(classes: 'mt-20 flex gap-8', [
+            // flex this container on large screens and stack on small screens
+            div([
+              ...steps.map((step) {
+                return div(classes: 'flex items-center gap-8 mb-12', [
+                  // icons
+                  img(src: '/svgs/${step['icon']}', alt: step['alt_text']!),
+                  div([
+                    // header
+                    h1(classes: 'font-bold text-3xl text-evergreen-900 mb-5', [.text(step['title']!)]),
+                    // description
+                    p(classes: 'text-charcoal-blue-700 text-2xl w-4/5', [
+                      .text(step['description']!),
+                    ]),
+                  ]),
+                ]);
+              }),
+              const AppButton(),
+            ]),
+            img(src: '/images/how_it_works.png', alt: 'How It Works Image', classes: 'w-1/2'),
+          ]),
         ],
       ),
     ]);
