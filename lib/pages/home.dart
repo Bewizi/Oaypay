8ce970e98f75d71d2ca1b430ee7530c8bca51663
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-04-22 22:50:15 +0100

    Refactor Home component layout for improved responsiveness and structure

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,8 +3,11 @@ import 'package:jaspr/jaspr.dart';
 import 'package:oaypay/components/app_button.dart';
 import 'package:oaypay/components/conatiner.dart';
 import 'package:oaypay/components/services.dart';
+import 'package:oaypay/pages/cta.dart';
 import 'package:oaypay/pages/do_more.dart';
+import 'package:oaypay/pages/footer.dart';
 import 'package:oaypay/pages/how_it_works.dart';
+import 'package:oaypay/pages/testtimonials.dart';
 import 'package:oaypay/pages/why_choose_us.dart';
 
 class Home extends StatelessComponent {
@@ -17,16 +20,19 @@ class Home extends StatelessComponent {
         AppConatiner(
           classes: 'py-20 ',
           children: [
-            section(classes: 'flex items-center justify-center gap-20', [
+            section(classes: 'flex flex-col md:flex-row items-center justify-center gap-20', [
               div([
-                span(classes: 'uppercase text-lime-cream-300 bg-pine-teal-800 py-3 px-4 rounded-full', [
-                  .text('all in one banking for everyone'),
-                ]),
+                span(
+                  classes: 'uppercase text-lime-cream-300 bg-pine-teal-800 py-3 px-4 rounded-full text-xs lg:text-base',
+                  [
+                    .text('all in one banking for everyone'),
+                  ],
+                ),
                 div(classes: 'mt-4', [
-                  h1(classes: 'text-6xl font-bold mb-6 text-white', [
+                  h1(classes: ' text-4xl md:text-6xl font-bold mb-6 text-white', [
                     .text('Simple solution to your financial problems'),
                   ]),
-                  p(classes: 'text-4xl text-white', [
+                  p(classes: 'text-xl md:text-4xl text-white', [
                     .text(
                       'with our secure online banking services, you can manage your finances from everywhere, at any time.',
                     ),
@@ -53,6 +59,9 @@ class Home extends StatelessComponent {
       const HowItWorks(),
       const WhyChooseUs(),
       const DoMore(),
+      const Testtimonials(),
+      const Cta(),
+      const Footer(),
     ]);
   }
 }
