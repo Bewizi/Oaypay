bfbb7730ed1765ff9b17ed830231ba4019ca172a
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2026-04-22 22:47:21 +0100

    Update AppButton component for responsive text size

--- lib/components/app_button.dart
+++ lib/components/app_button.dart
@@ -8,7 +8,7 @@ class AppButton extends StatelessComponent {
   Component build(BuildContext context) {
     return button(
       classes:
-          'text-dark-slate-grey-800 bg-lime-cream-300 hover:bg-lime-cream-400 py-5 px-12 rounded-full font-medium text-2xl mt-14',
+          'text-dark-slate-grey-800 bg-lime-cream-300 hover:bg-lime-cream-400 py-5 px-12 rounded-full font-medium md:text-2xl mt-14',
       [.text('Get started for free')],
     );
   }
